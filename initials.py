7a36afed0ee964cde2db80c0c8a7ae4d9af7cdb0
princekoney4-cyber <princekoney4@gmail.com>
2026-05-30 16:06:57 +0000

    assignment submition

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Prince Okpoti Koney
+# STUDENT ID:2425401464
+# Email: princekoney14@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Prince koney , this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials in normal size and large block letters
 
+    print()
+    print("...POK")
+    print()
+    print("PPPPPPPPPPPP           OOOOOOOOOO          KK        KK        ")
+    print("PP          PP       OO          OO        KK      KK          ")
+    print("PP          PP       OO          OO        KK    KK            ")
+    print("PP          PP       OO          OO        KK  KK              ")
+    print("PPPPPPPPPPPP         OO          OO        KKKK                ")
+    print("PP                   OO          OO        KK  KK              ")
+    print("PP                   OO          OO        KK    KK            ")
+    print("PP                   OO          OO        KK      KK          ")
+    print("PP              **   OO          OO   **   KK        KK      **")
+    print("PP              **     OOOOOOOOOO     **   KK          KK    **")
+    print()
 
 
 main()
